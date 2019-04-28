2d29a2db82b6709d081e1c3e5b731f6e6d9bca82
Renato Lessa da Silva <renatolessa.2011@hotmail.com>
2019-04-28 19:11:15 -0300

    Fazendo o app aproveitar o usuário logado na tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -25,10 +25,19 @@ export class HomePage {
   ionViewWillEnter() {
     this.menu.swipeEnable(false);
     }
+
     ionViewDidLeave() {
     this.menu.swipeEnable(true);
     }
+   ionViewDidEnter(){
+      this.auth.refreshToken()
+      .subscribe(response => {
+        this.auth.successfulLogin(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriasPage');
+  },
+  error => {});   
 
+   } 
     login() {    
          this.auth.authenticate(this.creds)
           .subscribe(response => {
